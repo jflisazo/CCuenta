83f766e988f6ab90f58b10a32a85dacb6ba7c86f
Fernando Lisazo <jflisazo@hotmail.com>
2020-10-06 12:01:44 -0300

    Ajustes varios

---
+++ CCuenta/CCuenta.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace CCuenta
+{
+    class CCuenta
+    {
+        //Atributos
+        private string nombre;
+        private string cuenta;
+        private double saldo;
+        private double tipoDeInteres;
+
+        //Propiedades y métodos
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    Console.WriteLine("Error: cadena vacía");
+                    return;
+                }
+                nombre = value;
+            }
+        }
+
+        public string Cuenta
+        {
+            get
+            {
+                return cuenta;
+            }
+            set
+            {
+                if (value.Length == 0)
+                {
+                    Console.WriteLine("Error: cuenta no válida");
+                    return;
+                }
+                cuenta = value;
+            }
+        }
+
+        public double Saldo
+        {
+            get
+            {
+                return saldo;
+            }
+        }
+
+        public void ingreso (double cantidad)
+        {
+            if (cantidad < 0)
+            {
+                Console.WriteLine("Error: cantidad negativa");
+                return;
+            }
+            saldo = saldo + cantidad;
+        }
+
+        public void reintegro (double cantidad)
+        {
+            if (saldo - cantidad == 0)
+            {
+                Console.WriteLine("Error: no dispone de saldo");
+            }
+            saldo = saldo - cantidad;
+        }
+
+        public double TipoDeInteres
+        {
+            get
+            {
+                return tipoDeInteres;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Error: tipo no válido");
+                    return;
+                }
+                tipoDeInteres = value;
+            }
+        }
+            
+            
+    }
+}
---
+++ CCuenta/CTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CCuenta
+{
+    class CTest
+    {
+        public static void Main()
+        {
+            CCuenta cuenta01 = new CCuenta();
+            cuenta01.Nombre = "Un nombre";
+            cuenta01.Cuenta = "Una cuenta";
+            cuenta01.TipoDeInteres = 2.5;
+
+            cuenta01.ingreso(12000);
+            cuenta01.reintegro(3000);
+
+            Console.WriteLine(cuenta01.Nombre);
+            Console.WriteLine(cuenta01.Cuenta);
+            Console.WriteLine(cuenta01.Saldo);
+            Console.WriteLine(cuenta01.TipoDeInteres);
+        }
+                
+    }
+}
